1ae53bd8892c46d0fb5895f15b73350bbbc633bd
Adriano Mota <adrianowsh@gmail.com>
2024-07-02 18:53:55 -0300

    finished algorithm

--- lib/ex_fizz_buzz.ex
+++ lib/ex_fizz_buzz.ex
@@ -1,18 +1,21 @@
 defmodule ExFizzBuzz do
-  @moduledoc """
-  Documentation for `ExFizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExFizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file()
   end
+
+  defp handle_file({:ok, file}) do
+    file
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&evaluate_number/1)
+  end
+
+  defp handle_file({:error, reason}), do: "Error reading the file: #{reason}"
+
+  defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizz_buzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number), do: number
 end
